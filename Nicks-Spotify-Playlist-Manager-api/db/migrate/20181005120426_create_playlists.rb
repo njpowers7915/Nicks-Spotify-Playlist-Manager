730bc5823a128320f87ccc2a54cb5708382d43a3
njpowers7915 <njpowers7915@gmail.com>
2018-10-05 08:05:40 -0400

    Created Track and Playlist models

---
+++ Nicks-Spotify-Playlist-Manager-api/db/migrate/20181005120426_create_playlists.rb
@@ -0,0 +1,12 @@
+class CreatePlaylists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :playlists do |t|
+      t.string :name
+      t.string :description
+      t.string :spotify_id
+      t.string :spotify_uri
+
+      t.timestamps
+    end
+  end
+end
