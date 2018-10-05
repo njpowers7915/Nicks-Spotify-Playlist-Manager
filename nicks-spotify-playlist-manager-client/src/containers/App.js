7417f94b800c325c80300510a251c8a0e15d10b6
njpowers7915 <njpowers7915@gmail.com>
2018-10-05 19:30:22 -0400

    Remove state from App Component

--- nicks-spotify-playlist-manager-client/src/containers/App.js
+++ nicks-spotify-playlist-manager-client/src/containers/App.js
@@ -5,18 +5,6 @@ import NavBar from './NavBar'
 import Playlists  from './Playlists'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      playlists: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/api/playlists')
-      .then(response => response.json())
-      .then(playlists => this.setState({playlists}))
-  }
   render() {
     return (
       <div className="App">
