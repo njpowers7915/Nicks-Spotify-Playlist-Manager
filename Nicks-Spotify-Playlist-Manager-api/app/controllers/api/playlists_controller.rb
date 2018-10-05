a68b2bb037ec85cba0dde793824c19ebe71e5a5d
njpowers7915 <njpowers7915@gmail.com>
2018-10-05 08:32:39 -0400

    Setup Playlists Controller

---
+++ Nicks-Spotify-Playlist-Manager-api/app/controllers/api/playlists_controller.rb
@@ -0,0 +1,48 @@
+class Api::PlaylistsController < ApplicationController
+
+  before_action :set_playlist, only: [:show, :update, :destroy]
+
+  def index
+    render json: Playlist.all
+  end
+
+  def create
+    playlist = Playlist.new(playlist_params)
+    if playlist.save
+      render json: playlist
+    else
+      render json: { message: playlist.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: @playlist
+  end
+
+  def update
+    if @playlist.update(playlist_params)
+      render json: @playlist
+    else
+      render json: { message: @playlist.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @playlist.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove this playlist" }, status: 400
+    end
+  end
+
+  private
+
+    def set_playlist
+      @playlist = Playlist.find_by(id: params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:name, :description)
+    end
+
+end
