9035b4b59fd48cd9dfc487cedab1290bde3939ba
njpowers7915 <njpowers7915@gmail.com>
2018-10-05 14:25:00 -0400

    Minor updates

--- nicks-spotify-playlist-manager-client/src/store.js
+++ nicks-spotify-playlist-manager-client/src/store.js
@@ -5,17 +5,13 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 
-//import playlists from './reducers/playlists';
+import CombineReducers from './reducers/CombineReducers';
 //import playlistFormData from './reducers/playlistFormData';
 
-const reducers = combineReducers({
-  playlists,
-  playlistFormData
-});
 const middleware = [thunk];
 
-//export default createStore(
-//  reducers,
-//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//  applyMiddleware(...middleware),
-//);
+export default createStore(
+  CombineReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
