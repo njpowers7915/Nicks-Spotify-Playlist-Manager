61c39111f5bf9e82439d83251859154a2c59e5f6
njpowers7915 <njpowers7915@gmail.com>
2018-10-05 13:46:37 -0400

    Created Playlist Actions file

---
+++ nicks-spotify-playlist-manager-client/src/actions/PlaylistsActions.js
@@ -0,0 +1,46 @@
+//import { resetPlaylistForm } from './newPlaylistForm';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+// ** Action Creators **
+const setPlaylists = playlists => {
+  return {
+    type: 'FETCH_PLAYLISTS_SUCCESS',
+    playlists
+  }
+}
+
+const addPlaylist = playlist => {
+  return {
+    type: 'CREATE_PLAYLIST_SUCCESS',
+    playlists
+  }
+}
+
+// ** Async Actions **
+export const fetchPlaylists = () => {
+  return dispatch => {
+    return fetch(`${API_URL}/playlists`)
+      .then(response => response.json())
+      .then(playlists => dispatch(setPlaylists(playlists)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const createPlaylist = playlist => {
+  return dispatch => {
+    return fetch(`${API_URL}/playlists`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ playlist: playlist })
+    })
+      .then(response => response.json())
+      .then(playlist => {
+        dispatch(addPlaylist(playlist))
+        //dispatch(resetPlaylistForm())
+      })
+      .catch(error => console.log(error))
+  }
+}
