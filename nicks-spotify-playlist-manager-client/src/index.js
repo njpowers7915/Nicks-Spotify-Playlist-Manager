fe2357711311e98edd3440f12dcb1e68f2b5771d
njpowers7915 <njpowers7915@gmail.com>
2018-10-06 10:37:01 -0400

    Created store and passed it to App through index.js

--- nicks-spotify-playlist-manager-client/src/index.js
+++ nicks-spotify-playlist-manager-client/src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-//import store from './store.js';
+import store from './store.js';
 
 
 ReactDOM.render(
-  <div className="app">
-    <App />
-  </div>,
-  document.getElementById('root')
-);
-
-/* ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-); */
+);
 
 registerServiceWorker();
--- nicks-spotify-playlist-manager-client/src/store.js
+++ nicks-spotify-playlist-manager-client/src/store.js
@@ -4,8 +4,31 @@ import {
   combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
+const PlaylistsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_PLAYLISTS-SUCCESS':
+      return state.playlists
+
+    default:
+      return state
+  }
+}
+
+const reducers = combineReducers({
+  playlists: PlaylistsReducer
+})
+const middleware = [thunk]
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware),
+);
+
+
+
 //import playlistFormData from './reducers/playlistFormData';
-import PlaylistsReducer from './reducers/PlaylistsReducer'
+/* import PlaylistsReducer from './reducers/PlaylistsReducer'
 
 const reducers = combineReducers({
   PlaylistsReducer
@@ -18,3 +41,5 @@ export default createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleware),
 );
+
+*/
