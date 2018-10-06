4aedc60ecd42bdb6ebbf598a284c69e89c52c0b7
njpowers7915 <njpowers7915@gmail.com>
2018-10-06 10:18:35 -0400

    New Playlist Form

---
+++ nicks-spotify-playlist-manager-client/src/components/NewPlaylistForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class NewPlaylistForm extends Component {
+
+  render() {
+    return(
+      <div>
+        Create New Playlist!
+        <form>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default NewPlaylistForm
