36b11ab9d70e983a4021d36e902419ff037c6801
njpowers7915 <njpowers7915@gmail.com>
2018-10-07 20:12:44 -0400

    Created Sessions Controller

---
+++ Nicks-Spotify-Playlist-Manager-api/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,42 @@
+class Api::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if !!auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      end
+      if @user = User.find_by(:email => auth['info']['email'])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        @user = User.create(:name => auth['info']['name'], :email => auth['info']['email'], :password => SecureRandom.hex)
+        session[:user_id] = @user.id
+        redirect_to @user
+      end
+    else
+      @user = User.find_by(:email => params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        flash[:danger] = 'Invalid name/password combination'
+        redirect_to login_path
+      end
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+end
--- Nicks-Spotify-Playlist-Manager-api/db/migrate/20181005121534_playlists_tracks.rb
+++ Nicks-Spotify-Playlist-Manager-api/db/migrate/20181005121534_playlists_tracks.rb
@@ -1,4 +1,4 @@
-class PlaylistsTracks < ActiveRecord::Migration[5.2]
+class CreatePlaylistsTracks < ActiveRecord::Migration[5.2]
   def change
     create_table :playlists_tracks do |t|
       t.integer :playlist_id
