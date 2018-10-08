b6d497da4598fe9650f5849c134c4d21e86f91a3
njpowers7915 <njpowers7915@gmail.com>
2018-10-07 20:16:38 -0400

    Made rails routes for login, logout, signup

--- Nicks-Spotify-Playlist-Manager-api/config/routes.rb
+++ Nicks-Spotify-Playlist-Manager-api/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :playlists
     resources :tracks
+    post 'signup', to: 'users#create'
+    post 'login', to: 'sessions#create'
+    post 'logout', to: 'sessions#destroy'
   end
 end
