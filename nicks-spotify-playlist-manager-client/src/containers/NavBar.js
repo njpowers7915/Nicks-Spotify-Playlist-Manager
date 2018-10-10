9892f29c52dd145f8d7a2afa58ef704b226139a3
njpowers7915 <njpowers7915@gmail.com>
2018-10-10 14:27:52 -0400

    Minor updates

--- nicks-spotify-playlist-manager-client/src/containers/NavBar.js
+++ nicks-spotify-playlist-manager-client/src/containers/NavBar.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-/* Add basic styling for NavLinks
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-} */
-/*
-<div className="NavBar" style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-  <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Pets!</NavLink>
-  <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Pet</NavLink>
-</div>
-*/
-
 const NavBar = () =>
   <div>
     <h3>Welcome user! -- <Link to="/playlists">Home</Link> -- Sign Out</h3>
