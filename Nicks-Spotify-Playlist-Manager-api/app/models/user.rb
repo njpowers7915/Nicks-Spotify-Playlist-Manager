3e93ca9c9497abc85e139ae1eb062a5482740d04
njpowers7915 <njpowers7915@gmail.com>
2018-10-11 08:50:07 -0400

    Added methods to User model that validate / invalidate tokens

--- Nicks-Spotify-Playlist-Manager-api/app/models/user.rb
+++ Nicks-Spotify-Playlist-Manager-api/app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   has_many :playlists
   has_many :tracks, through: :playlists
 
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(username, password)
+    user = find_by(username: username)
+    if user && user.authenticate(password)
+      user
+    end
+  end
+
 end
