d9b91bc50d1cc9a16bfd7f4dd93c86d4535159ad
njpowers7915 <njpowers7915@gmail.com>
2018-10-11 18:21:55 -0400

    Updating Logout Button

---
+++ nicks-spotify-playlist-manager-client/src/components/LogoutButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LogoutButton = () =>
+  <div className="LogoutButton">
+    <h3>Logout Button</h3>
+  </div>
+
+export default LogoutButton
