8a768d3fae6c5b281563244dcd78ce24317e6b77
njpowers7915 <njpowers7915@gmail.com>
2018-10-11 14:37:53 -0400

    Updating playlists route under users controller

--- Nicks-Spotify-Playlist-Manager-api/app/controllers/api/users_controller.rb
+++ Nicks-Spotify-Playlist-Manager-api/app/controllers/api/users_controller.rb
@@ -8,7 +8,11 @@ class Api::UsersController < ApiController
 
   def playlists
     user = User.find_by_auth_token!(request.headers[:token])
-    render json: { user: { username: user.username, email: user.email, name: user.name }}
+    user_playlists = Playlist.where(user_id: user.id)
+    render json: {
+      user: { username: user.username, email: user.email, name: user.name },
+      playlists: user_playlists
+    }
   end
 
   private
@@ -16,5 +20,5 @@ class Api::UsersController < ApiController
   def user_params
     params.require(:user).except(:username, :password, :name, :email)
   end
-  
+
 end
