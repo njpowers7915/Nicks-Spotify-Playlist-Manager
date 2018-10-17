9be9cdf3270fb01ff8853b8f715082daf388a9c2
njpowers7915 <njpowers7915@gmail.com>
2018-10-17 13:43:44 -0400

    Updated NavBar

--- nicks-spotify-playlist-manager-client/src/containers/NavBar.js
+++ nicks-spotify-playlist-manager-client/src/containers/NavBar.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogoutButton from '../components/LogoutButton'
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { logout } from '../actions/authActions'
+//import LogoutButton from '../components/LogoutButton'
 
+class NavBar extends React.Component {
+  handleLogout = (event) = {
+    event.preventDefault();
+    this.props.logout();
+    this.props.history.push('/')
+  }
+
+  render() {
+    const loggedInNav = (
+      <nav>
+        <Link to="/playlists">Home</Link>
+        <button onClick={(e) => this.handleLogout(e)}>Log Out</button>
+      </nav>
+    )
+    return (
+      <div>{this.props.isAuthenticated ? loggedInNav : null}</div>
+    )
+  }
+}
+
+export default NavBar = withRouter(connect(null, {logout})(NavBar))
+/*
 const NavBar = ({handleLogout}) =>
   <div className="NavBar">
     <h3>Welcome user! -- <Link to="/playlists">Home</Link> -- <LogoutButton handleLogout={handleLogout} /></h3>
 
-    {/* {<NavLink to="/" exact style={link}
+     {<NavLink to="/" exact style={link}
       activeStyle={{background: 'darkblue'}}>Home</NavLink>
     <NavLink to="/about" exact style={link}
-      activeStyle={{background: 'darkblue'}}>Logout</NavLink> */}
+      activeStyle={{background: 'darkblue'}}>Logout</NavLink>
   </div>;
 
 export default NavBar
+*/
