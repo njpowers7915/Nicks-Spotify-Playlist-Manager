d406ea95055c10f704576a19a0a8817caac429a2
njpowers7915 <njpowers7915@gmail.com>
2018-10-17 13:14:03 -0400

    Updating AuthReducer

--- nicks-spotify-playlist-manager-client/src/reducers/root.js
+++ nicks-spotify-playlist-manager-client/src/reducers/root.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import playlistsReducer from './playlistsReducer';
 import tracksReducer from './tracksReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
   playlists: playlistsReducer,
-  tracks: tracksReducer
+  tracks: tracksReducer,
+  auth: authReducer
 })
