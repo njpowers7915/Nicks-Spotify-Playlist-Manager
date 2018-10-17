1c9f9f1724e235cce1767114ea4b0cf391c53d94
njpowers7915 <njpowers7915@gmail.com>
2018-10-17 12:59:56 -0400

    Updated users controller

--- Nicks-Spotify-Playlist-Manager-api/app/controllers/api/users_controller.rb
+++ Nicks-Spotify-Playlist-Manager-api/app/controllers/api/users_controller.rb
@@ -1,36 +1,43 @@
 class Api::UsersController < ApiController
-  before_action :authenticate, except: [:create]
+  before_action :authenticate_user, except: [:create]
+  before_action :set_user, only: [:show, :update, :destroy]
 
-  def new
-    @user = User.new
+  def index
+    @users = User.all
+    render json: @users
   end
 
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   def create
     user = User.new(user_params)
-    if user.save
-      render json: {}, status: 200
+    if @user.valid? && @user.save
+      render json: @user, status: 200
     else
-      render json: {
-        error: "Could not create user",
-        status: 422
-      }, status: 422
+      render json: {errors: "Could not create user"}
     end
   end
 
-  def profile
-    user = User.find_by_auth_token!(request.headers[:token])
-    user_playlists = Playlist.where(user_id: user.id)
-    render json: {
-      user: { username: user.username, email: user.email },
-      playlists: user_playlists
-    }
+  def destroy
+    @user.delete
   end
 
+#  def profile
+#    user = User.find_by_auth_token!(request.headers[:token])
+#    user_playlists = Playlist.where(user_id: user.id)
+#    render json: {
+#      user: { username: user.username, email: user.email },
+#      playlists: user_playlists
+#    }
+#  end
+
   private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
 
   def user_params
     params.require(:user).except(:username, :email, :password)
