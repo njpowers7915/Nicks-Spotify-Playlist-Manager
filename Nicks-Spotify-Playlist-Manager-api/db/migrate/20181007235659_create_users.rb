74854d714ab35b103198eccf22f1b47284112226
njpowers7915 <njpowers7915@gmail.com>
2018-10-17 13:09:29 -0400

    Finished updating Rails Backend to handle authentication with Knock Gem

--- Nicks-Spotify-Playlist-Manager-api/db/migrate/20181007235659_create_users.rb
+++ Nicks-Spotify-Playlist-Manager-api/db/migrate/20181007235659_create_users.rb
@@ -4,8 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :username
       t.string :email
       t.string :password_digest
-      t.string :uid
-      t.string :auth_token
 
       t.timestamps
     end
